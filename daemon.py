a50594a00a53d56703a6f340fc2f95ac06a535cf
Jordi Canals <jordicanalsros@gmail.com>
2015-02-04 13:31:38 +0100

    add init code

---
+++ daemon.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+
+"""
+Posted On: https://linuxdevmag.wordpress.com/2010/05/18/how-to-create-a-linux-daemon/
+Thanks jajcarmona
+"""
+
+import sys, os, time, atexit
+from signal import SIGTERM
+
+class Daemon:
+    """
+    A generic daemon class.
+
+    Usage: Subclass this class and override the 'run' method
+    """
+    def __init__(self, pidfile, stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
+        self.stdin = stdin
+        self.stdout = stdout
+        self.stderr = stderr
+        self.pidfile = pidfile
+
+    def daemonize(self):
+        """
+        Do the 'UNIX double-fork magic'
+        """
+        try:
+            pid = os.fork()
+            if pid > 0:
+                # Exit first parent
+                sys.exit(0)
+        except OSError, e:
+            sys.stderr.write("Fork #1 failed: %d (%s)\n" % (e.errno, e.strerror))
+            sys.exit(1)
+
+        # Decouple from parent environment
+        os.chdir("/")
+        os.setsid()
+        os.umask(0)
+
+        # Do second fork
+        try:
+            pid = os.fork()
+            if pid > 0:
+                # Exit from second parent
+                sys.exit(0)
+        except OSError, e:
+            sys.stderr.write("Fork #2 failed: %d (%s)\n" % (e.errno, e.strerror))
+            sys.exit(1)
+
+        # Redirect standard file descriptors
+        sys.stdout.flush()
+        sys.stderr.flush()
+        si = file(self.stdin, 'r')
+        so = file(self.stdout, 'a+')
+        se = file(self.stderr, 'a+', 0)
+        os.dup2(si.fileno(), sys.stdin.fileno())
+        os.dup2(so.fileno(), sys.stdout.fileno())
+        os.dup2(se.fileno(), sys.stderr.fileno())
+
+        # Write pidfile
+        atexit.register(self.delpid)
+        pid = str(os.getpid())
+        file(self.pidfile,'w+').write("%s\n" % pid)
+
+    def delpid(self):
+        os.remove(self.pidfile)
+
+    def start(self):
+        # Check for a pidfile to see if the daemon already runs
+        try:
+            pf = file(self.pidfile,'r')
+            pid = int(pf.read().strip())
+            pf.close()
+        except IOError:
+            pid = None
+
+        if pid:
+            message = "Pidfile %s already exist. Daemon already running?\n"
+            sys.stderr.write(message % self.pidfile)
+            sys.exit(1)
+
+        # Start the daemon
+        self.daemonize()
+        self.run()
+
+    def stop(self):
+        # Get the pid from the pidfile
+        try:
+            pf = file(self.pidfile,'r')
+            pid = int(pf.read().strip())
+            pf.close()
+        except IOError:
+            pid = None
+
+        if not pid:
+            message = "Pidfile %s does not exist. Daemon not running?\n"
+            sys.stderr.write(message % self.pidfile)
+            return # not an error in a restart
+
+        # Try killing the daemon process
+        try:
+            while 1:
+                os.kill(pid, SIGTERM)
+                time.sleep(0.1)
+        except OSError, err:
+            err = str(err)
+            if err.find("No such process") > 0:
+                if os.path.exists(self.pidfile):
+                    os.remove(self.pidfile)
+            else:
+                print str(err)
+                sys.exit(1)
+
+    def restart(self):
+        self.stop()
+        self.start()
+
+    def run(self):
+        """
+        You should override this method when you subclass Daemon. It will be called after the process has been
+        daemonized by start() or restart().
+        """
---
+++ microfanPi.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+"""
+To start the daemon, we should run the next command:
+python microfanPi.py start
+
+To stop the daemon, we should run the next command:
+python microfanPi.py stop
+
+To restart the daemon, we should run the next command:
+python microfanPi.py restart
+"""
+
+import sys, time, os
+import RPi.GPIO as GPIO
+from daemon import Daemon
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+#FAN
+FAN_PIN = 4		# Set GPIO pin the fan are connected to
+FAN_ON = 35.0	# Set Temperature the fan put on
+FAN_OFF = 32.0	# Set Temperature the fan put off
+FAN_ITVAL = 10	# Set interval the fan verify temperature (in sec)
+
+#Temperature
+TEMP_FILE = 'cat /sys/class/thermal/thermal_zone0/temp'
+
+# Set the fan pin as output pins
+GPIO.setup(FAN_PIN, GPIO.OUT)
+
+
+class MyFan(Daemon):
+    def run(self):
+		FanOff()
+        while True:
+			fSensor = os.popen(TEMP_FILE)
+            temp = float(fSensor.read()) * 0.001
+            fSensor.close()
+			
+			if temp <= FAN_OFF:
+				# Stop fan, it's cold!!
+				GPIO.output(FAN_PIN, GPIO.LOW)
+            else:
+                if temp >= FAN_ON:
+                    # We have warmed up enough, turn the fan on
+                    GPIO.output(FAN_PIN, GPIO.HIGH)
+			
+            time.sleep(FAN_ITVAL)
+	
+	# Function to set all drives off
+	def FanOff():
+		GPIO.output(FAN_PIN, GPIO.LOW)
+		
+
+if __name__ == "__main__":
+    daemon = MyDaemon('/tmp/daemon-example.pid')
+    if len(sys.argv) == 2:
+        if 'start' == sys.argv[1]:
+            daemon.start()
+        elif 'stop' == sys.argv[1]:
+            daemon.stop()
+        elif 'restart' == sys.argv[1]:
+            daemon.restart()
+        else:
+            print "Unknown command"
+            sys.exit(2)
+        sys.exit(0)
+    else:
+        print "Usage: %s start|stop|restart" % sys.argv[0]
+        sys.exit(2)
