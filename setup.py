c69dd0e71d0af7edeef24d284ddbc4556e02cdbe
Jordi Canals <jordicanalsros@gmail.com>
2015-02-13 00:16:24 +0100

    add more info to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,23 @@
 from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+
+# Install
+# python setup.py install --record files.txt
+
+# Uninstall
+# pip uninstall microfanPi
+
+class CustomInstallCommand(install):
+    """Customized setuptools install command - prints a friendly greeting."""
+    def run(self):
+        print "Dualion Power!"
+        install.run(self)
+        #post-processing code
+		
 
 try:
-    long_description = open("README.rst").read()
+    long_description = open("README.md").read()
 except IOError:
     long_description = ""
 
@@ -10,7 +26,11 @@ setup(
     version="0.1.0",
     description="Software to control fan",
     author="Dualion",
+	author_email = "admin@dualion.com",
+    url = "http://dualion.com/",
+	
     packages=['microfanPi'],
+	exclude_package_data = { '': ['.gitignore','README.md','LICENSE'] },
     install_requires=[],
     long_description=long_description,
     entry_points={
@@ -21,7 +41,4 @@ setup(
     classifiers=[
         'Programming Language :: Python :: 2.7',
     ]
-
-
-
 )
