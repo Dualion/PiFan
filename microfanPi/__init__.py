fe9de5251d3bbd9fe72841e18a0df91835ce7775
lluiscanals <lluiscanalsros@gmail.com>
2015-02-13 21:44:12 +0100

    Creo rama develop. Separo clase myfan.

--- microfanPi/__init__.py
+++ microfanPi/__init__.py
@@ -11,53 +11,12 @@ To restart the daemon, we should run the next command:
 python microfanPi.py restart
 """
 
-import sys, time, os
-import RPi.GPIO as GPIO
-from daemon import Daemon
+import sys
+import myfan
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
-
-# FAN
-FAN_PIN = 11    # Set GPIO pin the fan are connected to
-FAN_ON = 35   # Set Temperature the fan put on
-FAN_OFF = 30  # Set Temperature the fan put off
-FAN_ITVAL = 10  # Set interval the fan verify temperature (in sec)
-
-# Temperature
-TEMP_FILE = 'cat /sys/class/thermal/thermal_zone0/temp'
-
-# Set the fan pin as output pins
-GPIO.setup(FAN_PIN, GPIO.OUT)
-
-
-class MyFan(Daemon):
-    def run(self):
-        self.fan_off()
-        sys.stderr.write("In Run")
-        sys.stdout.flush()
-        sys.stderr.flush()
-
-        while True:
-            f_sensor = os.popen(TEMP_FILE)
-            temp = float(f_sensor.read()) * 0.001
-            f_sensor.close()
-
-            if temp <= FAN_OFF:
-                # Stop fan, it's cold!!
-                GPIO.output(FAN_PIN, GPIO.LOW)
-            if temp >= FAN_ON:
-                # We have warmed up enough, turn the fan on
-                GPIO.output(FAN_PIN, GPIO.HIGH)
-
-            time.sleep(FAN_ITVAL)
-
-    # Function to set all drives off
-    def fan_off(self):
-        GPIO.output(FAN_PIN, GPIO.LOW)
 
 def fan():
-    daemon = MyFan('/var/run/microfanPi.pid')
+    daemon = myfan.MyFan('/var/run/microfanPi.pid')
     if len(sys.argv) == 2:
         if 'start' == sys.argv[1]:
             daemon.start()
